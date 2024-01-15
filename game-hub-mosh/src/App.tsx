34d673b539453d2e3ad65cf8d2fe47f0feafa171
lustempu <lstempuzis@gmail.com>
2024-01-15 14:20:54 -0500

    build a responsive layout

--- game-hub-mosh/src/App.tsx
+++ game-hub-mosh/src/App.tsx
@@ -1,7 +1,26 @@
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
-  return <Button colorScheme="blue">Button</Button>;
+  return (
+    <Grid
+      templateAreas={{
+        base: '"nav" " main"', //layout for small screens (mobile phones)
+        lg: '"nav nav" "aside main"', //layout for large screens (1024px)
+      }}
+    >
+      <GridItem area="nav" bg="coral">
+        Nav
+      </GridItem>
+      <Show above="lg">
+        <GridItem area="aside" bg="gold">
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem area="main" bg="dodgerblue">
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;

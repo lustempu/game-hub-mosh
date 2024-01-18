2c57fda2b13a7477e285c6d4e31976c323410259
lustempu <lstempuzis@gmail.com>
2024-01-18 13:26:23 -0500

    Create custom hook to fetch the games

--- game-hub-mosh/src/components/GameGrid.tsx
+++ game-hub-mosh/src/components/GameGrid.tsx
@@ -1,33 +1,8 @@
-import React, { useEffect, useState } from "react";
-import apiClient from "../services/api-client";
 import { Text } from "@chakra-ui/react";
-
-interface Game {
-  id: number;
-  name: string;
-}
-
-interface FetchGamesResponse {
-  count: number;
-  results: Game[];
-}
+import useGames from "../hooks/useGames";
 
 const GameGrid = () => {
-  const [games, setGames] = useState<Game[]>([]); // to retrieve the games from the database
-  const [error, setError] = useState(""); // to display an error message if the games cannot be retrieved
-
-  //using effect hook to retrieve the games from the backened
-  useEffect(() => {
-    apiClient
-      .get<FetchGamesResponse>("/games")
-      .then((response) => {
-        setGames(response.data.results); // here we will need to use an TS interface to define the shape of the response object
-      })
-      .catch((error) => {
-        setError(error.message);
-      });
-  }, []);
-
+  const { games, error } = useGames();
   return (
     <>
       {error && <Text>{error}</Text>}
---
+++ game-hub-mosh/src/hooks/useGames.ts
@@ -0,0 +1,41 @@
+import { useState, useEffect, } from "react";
+import apiClient from "../services/api-client";
+import { CanceledError } from "axios";
+
+
+interface Game {
+    id: number;
+    name: string;
+  }
+  
+  interface FetchGamesResponse {
+    count: number;
+    results: Game[];
+  }
+
+const useGames= () => {
+    const [games, setGames] = useState<Game[]>([]); // to retrieve the games from the database
+    const [error, setError] = useState(""); // to display an error message if the games cannot be retrieved
+  
+    //using effect hook to retrieve the games from the backened
+    useEffect(() => {
+        const controller = new AbortController();// to cancel the request if the component is unmounted
+
+      apiClient
+        .get<FetchGamesResponse>("/games", {signal: controller.signal})//signal property is used to cancel the request if the component is unmounted
+        .then((response) => {
+          setGames(response.data.results); // here we will need to use an TS interface to define the shape of the response object
+        })
+        .catch((error) => {
+            if(error instanceof CanceledError) return;
+          {setError(error.message)};
+        });
+        return () => {// to cancel the request if the component is unmounted 
+            controller.abort();
+        }
+    }, []);// array of dependecies to make sure that the effect is only run once and not on every re-render
+  return {games, error};
+} 
+
+
+export default useGames;

db59be74098f75d45c627cc5c13ec484e84c2d9a
lustempu <lstempuzis@gmail.com>
2024-01-23 13:55:15 -0500

    Show Loading Skeletons

--- game-hub-mosh/src/hooks/useGames.ts
+++ game-hub-mosh/src/hooks/useGames.ts
@@ -24,25 +24,27 @@ export interface Game {
 const useGames= () => {
     const [games, setGames] = useState<Game[]>([]); // to retrieve the games from the database
     const [error, setError] = useState(""); // to display an error message if the games cannot be retrieved
-  
+  //to track the loading state for the gameCard skeleton
+    const [isLoading, setLoading]= useState(false);
+
     //using effect hook to retrieve the games from the backened
     useEffect(() => {
         const controller = new AbortController();// to cancel the request if the component is unmounted
-
+        setLoading(true);
       apiClient
         .get<FetchGamesResponse>("/games", {signal: controller.signal})//signal property is used to cancel the request if the component is unmounted
-        .then((response) => {
-          setGames(response.data.results); // here we will need to use an TS interface to define the shape of the response object
-        })
+        .then((response) => {setGames(response.data.results);
+        setLoading(false)})// here we will need to use an TS interface to define the shape of the response object
         .catch((error) => {
             if(error instanceof CanceledError) return;
-          {setError(error.message)};
+           setError(error.message);
+           setLoading(false);
         });
         return () => {// to cancel the request if the component is unmounted 
             controller.abort();
         }
     }, []);// array of dependecies to make sure that the effect is only run once and not on every re-render
-  return {games, error};
+  return {games, error, isLoading};
 } 
 
 

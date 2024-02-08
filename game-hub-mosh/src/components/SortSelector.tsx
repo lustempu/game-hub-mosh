0b66f892de790e4f7d7a7bb22220d6ddc3eea00e
lustempu <lstempuzis@gmail.com>
2024-02-08 12:28:08 -0500

    Build Sort Selector

---
+++ game-hub-mosh/src/components/SortSelector.tsx
@@ -0,0 +1,24 @@
+import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
+import { BsChevronDown } from "react-icons/bs";
+
+const SortSelector = () => {
+  return (
+    <>
+      <Menu>
+        <MenuButton as={Button} rightIcon={<BsChevronDown />}>
+          {"Order by Relavance"}
+        </MenuButton>
+        <MenuList>
+          <MenuItem>Relavence</MenuItem>
+          <MenuItem>Date Added</MenuItem>
+          <MenuItem>Name</MenuItem>
+          <MenuItem>Release Date</MenuItem>
+          <MenuItem>Popularity</MenuItem>
+          <MenuItem>Average Rating</MenuItem>
+        </MenuList>
+      </Menu>
+    </>
+  );
+};
+
+export default SortSelector;
